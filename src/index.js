1bdfea88b3d736e77070c24f0e247fe5cbb4f37f
Anatoly <anatoly.gusakovsky@gmail.com>
2023-09-22 11:32:36 +0300

    config upd

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import Koa from 'koa';
 import {router} from "./routes/tasks_routes.js";
 import bodyParser from 'koa-bodyparser';
 import cors from '@koa/cors';
+import {PORT} from "./config.js";
 
-// import dotenv from 'dotenv'
-// dotenv.config()
 
 const app = new Koa();
 app.use(cors())
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
-// app.listen(process.env.PORT);
-// console.log(`Application is running on port ${process.env.PORT}`);
-app.listen(3000);
-console.log(`Application is running on port ${3000}`);
+
+app.listen(PORT);
+console.log(`Application is running on port ${PORT}`);
